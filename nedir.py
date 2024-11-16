29ce906b7144e9aeea35479605c2e9610f9e8a7d
nataliemeow <ataberkzxc@gmail.com>
2024-11-16 12:36:14 +0300

    get real

--- nedir.py
+++ nedir.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python3
 
+#--start of config--#
+
+# Don't use Unicode characters.
+ASCII_ONLY = False
+# Don't use terminal color escapes.
+COLORLESS = False
+# Escape Turkish letters with a preceding symbol. `None` for no escaping.
+ESCAPE_TURKISH_LETTERS = '/'
+# User agent string to query the service with.
+LIES_AND_DECEIT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
+
+#--end of config--#
+
 # Nedir - A command-line tool to query the TDK Güncel Türkçe Sözlük.
 # Written in 2023 by Natalie <razorlovesbaba@tuta.io>
 #
@@ -18,61 +31,60 @@ from urllib.request import Request, urlopen
 from urllib.parse import quote
 from json import loads
 
-# Don't use Unicode characters.
-ASCII_ONLY             = False
-# Don't use terminal color escapes.
-COLORLESS              = False
-# Escape Turkish letters with a symbol before. Can be `None` for no escaping.
-ESCAPE_TURKISH_LETTERS = '/'
-# User agent string to query the service with.
-LIES_AND_DECEIT        = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
-
-if not COLORLESS:
-    RESET = '\x1b[0m'
-    FAINT = '\x1b[2m'
-    COLOR = '\x1b[1;31m'
+if COLORLESS:
+	RESET = ''
+	FAINT = ''
+	ACCENT = ''
+	ERROR = ''
+else:
+	RESET = '\x1b[0m'
+	FAINT = '\x1b[2m'
+	ACCENT = '\x1b[1;91m'
+	ERROR = '\x1b[1;31m'
 
 SYMBOLS = {
-    'i': 'ı',
-    'o': 'ö',
-    'u': 'ü',
+	'c': 'ç',
+	'g': 'ğ',
+	'i': 'ı',
+	'o': 'ö',
+	's': 'ş',
+	'u': 'ü',
 }
 
 if len(argv) == 1:
-    word = input('\x1b[1;31mkelime:\x1b[0m ')
-    print(f'\x1b[2m{("-" if ASCII_ONLY else "—") * (len(word) + 8)}{RESET}')
+	word = input(f'{ACCENT}kelime:{RESET} ')
+	print(f'{FAINT}{('-' if ASCII_ONLY else '—') * (len(word) + 8)}{RESET}')
 else:
-    word = ' '.join(argv[1:])
+	word = ' '.join(argv[1:])
 
 if ESCAPE_TURKISH_LETTERS:
-    for symbol, new_symbol in SYMBOLS.items():
-        word = word.replace(ESCAPE_TURKISH_LETTERS + symbol, new_symbol)
+	for symbol, new_symbol in SYMBOLS.items():
+		word = word.replace(ESCAPE_TURKISH_LETTERS + symbol, new_symbol)
 
 req = Request(f'https://sozluk.gov.tr/gts?ara={quote(word)}')
 req.add_header('User-Agent', LIES_AND_DECEIT)
 with urlopen(req) as response:
-    body = response.read()
-    data = loads(body.decode('utf-8'))
+	body = response.read()
+	data = loads(body.decode('utf-8'))
 
 if type(data) == dict and (error := data.get('error')):
-    print(f'{COLOR}{error}{RESET}')
-    quit()
+	print(f'{ERROR}{error}{RESET}')
+	quit()
 
 for index, entry in enumerate(data):
-    if index != 0: print()
-    print(COLOR, end='')
-    if len(data) > 1:
-        print(f'{index + 1}. ', end='')
-    print(f'{entry["madde"]}:{RESET}', end='')
-    print()
+	if index != 0: print()
+	print(ACCENT, end='')
+	if len(data) > 1: print(f'{index + 1}. ', end='')
+	print(f'{entry["madde"]}:{RESET}', end='')
+	print()
 
-    for meaning in entry['anlamlarListe']:
-        print(f' {COLOR}-{RESET} {meaning["anlam"]}')
-        if examples := meaning.get('orneklerListe'):
-            for example in examples:
-                print(f'   {COLOR}>{RESET} {example["ornek"]}')
+	for meaning in entry['anlamlarListe']:
+		print(f'{ACCENT}-{RESET} {meaning["anlam"]}')
+		if examples := meaning.get('orneklerListe'):
+			for example in examples:
+				print(f'  {ACCENT}>{RESET} {example["ornek"]}')
 
-                if not example.get('yazar'): continue
-                writer = example['yazar'][0]['tam_adi']
-                print(f'     {FAINT}- {",".join(writer["tam_adi"] for writer in example["yazar"])}', end='')
-                print('\x1b[0m')
+				if not example.get('yazar'): continue
+				writer = example['yazar'][0]['tam_adi']
+				print(f'    {FAINT}- {",".join(writer["tam_adi"] for writer in example["yazar"])}', end='')
+				print(RESET)
